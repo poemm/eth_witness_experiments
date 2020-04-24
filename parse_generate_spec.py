024c7fb1439af2a67273e2674d2ebae01fcb0e63
poemm <36397285+poemm@users.noreply.github.com>
2020-04-24 14:03:01 -0500

    merge account and storage tree nodes

--- parse_generate_spec.py
+++ parse_generate_spec.py
@@ -8,13 +8,14 @@ verbose = 0
 # base stuff
 
 def parse_Byte(bytes_, idx):
+  #if verbose: print("parse_Byte",bytes_, idx)
   assert len(bytes_)>idx
   byte = bytes_[idx]
   idx += 1
   return idx, byte
 
 def gen_Byte(file_, byte):
-  if verbose: print("gen_byte",file_,byte)
+  #if verbose: print("gen_byte",file_,byte)
   #file_.write(byte)
   file_.append(byte)
 
@@ -97,7 +98,7 @@ def gen_Byte_Lower_Nibble_Zero(file_, b):
   gen_Byte(file_,b)
 
 def parse_Nibbles(bytes_, idx, nibbleslen):
-  if verbose: print("parse_Nibbles",bytes_,idx,nibbleslen)
+  if verbose: print("parse_Nibbles",idx,nibbleslen)
   assert nibbleslen <= 64
   nibbles = bytearray([])
   for i in range(nibbleslen//2):
@@ -147,7 +148,7 @@ def parse_Tree(bytes_,idx):
   idx, terminal_bb = parse_Byte(bytes_, idx)
   assert terminal_bb == 0xbb
   idx,c = parse_Metadata(bytes_,idx)
-  idx,w = parse_Tree_Node(bytes_,idx,0)
+  idx,w = parse_Tree_Node(bytes_,idx,0,0)
   return idx, (c,w)
 
 def gen_Tree(file_, w):
@@ -169,19 +170,20 @@ def gen_Metadata(file_, c):
 #################################
 # tree nodes for world state tree
 
-def parse_Tree_Node(bytes_, idx, d):
+def parse_Tree_Node(bytes_, idx, d, s):
+  if verbose: print("parse_Tree_Node", idx, d, s)
   assert d<65
   idx, nodetype = parse_Byte(bytes_, idx)
   assert nodetype in {0x00,0x01,0x02,0x03}
   if nodetype==0x00:
-    idx, branch = parse_Branch_Node(bytes_, idx, d)
+    idx, branch = parse_Branch_Node(bytes_, idx, d, s)
     return idx, branch
   elif nodetype==0x01:
-    idx, extension = parse_Extension_Node(bytes_, idx, d)
+    idx, extension = parse_Extension_Node(bytes_, idx, d, s)
     return idx, extension
   elif nodetype==0x02:
-    idx, account = parse_Account_Node(bytes_, idx, d)
-    return idx, account
+    idx, leaf = parse_Leaf_Node(bytes_, idx, d, s)
+    return idx, leaf
   elif nodetype==0x03:
     idx, hash_ = parse_Bytes32(bytes_, idx)
     return idx, hash_
@@ -194,24 +196,24 @@ def gen_Tree_Node(file_, node):
   elif node[0]=="extension":
     gen_Byte(file_, 0x01)
     gen_Extension_Node(file_, node)
-  elif node[0]=="account":
+  elif node[0]=="leaf":
     gen_Byte(file_, 0x02)
-    gen_Account_Node(file_, node)
+    gen_Leaf_Node(file_, node)
   elif node[0]=="hash":
     gen_Byte(file_, 0x03)
     gen_Bytes32(file_, node[1])
   else:
     print("ERROR")
 
-def parse_Branch_Node(bytes_, idx, depth):
-  if verbose: print("parse_Branch_Node",idx,depth)
+def parse_Branch_Node(bytes_, idx, depth, storage_flag):
+  if verbose: print("parse_Branch_Node",idx,depth, storage_flag)
   assert depth < 64
   idx, bitmask = parse_Bytes2_More_Than_One_Bit_Set(bytes_, idx)
   bitmaskstr = bin(bitmask[0])[2:].zfill(8) + bin(bitmask[1])[2:].zfill(8)
   c = [None]*16
   for i in range(16):
     if bitmaskstr[i]=='1':
-      idx, child = parse_Tree_Node(bytes_, idx, depth+1)
+      idx, child = parse_Tree_Node(bytes_, idx, depth+1, storage_flag)
       c[i] = child
   return idx, ("branch", c)
 
@@ -231,11 +233,12 @@ def gen_Branch_Node(file_,b):
     if bitmaskstr[i]=='1':
       gen_Tree_Node(file_,b[i+1])
 
-def parse_Extension_Node(bytes_, idx, depth):
+def parse_Extension_Node(bytes_, idx, depth, storage_flag):
+  if verbose: print("parse_Extension_Node", idx, depth, storage_flag)
   assert depth < 63
   idx, nibbleslen = parse_Byte(bytes_, idx)
   idx, nibbles = parse_Nibbles(bytes_, idx, nibbleslen)
-  idx, child = parse_Child_Of_Extension_Node(bytes_, idx, depth+nibbleslen)
+  idx, child = parse_Child_Of_Extension_Node(bytes_, idx, depth+nibbleslen, storage_flag)
   # if odd number of nibbles, last one must be zeros
   return idx, ("extension", (nibbleslen, nibbles), child)
 
@@ -245,13 +248,13 @@ def gen_Extension_Node(file_,e):
   gen_Nibbles(file_, e[1][1])
   gen_Child_Of_Extension_Node(file_,e[2])
 
-def parse_Child_Of_Extension_Node(bytes_, idx, depth):
+def parse_Child_Of_Extension_Node(bytes_, idx, depth, storage_flag):
   if verbose: print("parse_Child_Of_Extension_Node",bytes_,idx,depth)
   assert depth<65
   idx, nodetype = parse_Byte(bytes_, idx)
   assert nodetype in {0x00,0x03}
   if nodetype==0x00:
-    idx, branch = parse_Branch_Node(bytes_, idx, depth)
+    idx, branch = parse_Branch_Node(bytes_, idx, depth, storage_flag)
     return idx, branch
   elif nodetype==0x03:
     idx, hash_ = parse_Bytes32(bytes_, idx)
@@ -265,8 +268,22 @@ def gen_Child_Of_Extension_Node(file_, node):
     gen_Byte(file_, 0x03)
     gen_Bytes32(file_, node[1])
 
-def parse_Account_Node(bytes_,idx, depth):
+def parse_Leaf_Node(bytes_,idx, depth, storage_flag):
+  if verbose: print("parse_Leaf_Node", idx, depth, storage_flag)
   assert depth<65
+  if storage_flag == 0: # leaf of account tree
+    return parse_Account_Node(bytes_,idx,depth)
+  else: # leaf of storage tree
+    return parse_Storage_Leaf_Node(bytes_,idx,depth)
+
+def gen_Leaf_Node(file_, leaf):
+  if verbose: print("gen_Leaf_Node",file_,leaf)
+  if len(leaf)>4:
+    gen_Account_Node(file_, leaf)
+  else:
+    gen_Storage_Leaf_Node(file_, leaf)
+
+def parse_Account_Node(bytes_,idx,depth):
   idx, accounttype = parse_Byte(bytes_, idx)
   assert accounttype in {0x00,0x01}
   idx, pathnibbles = parse_Nibbles(bytes_,idx,64-depth)
@@ -275,23 +292,22 @@ def parse_Account_Node(bytes_,idx, depth):
   idx, balance = parse_Bytes32(bytes_,idx)
   if accounttype == 0x01:
     idx, bytecode = parse_Bytecode(bytes_,idx)
-    idx, storage = parse_Account_Storage_Tree_Node(bytes_,idx,0)
+    idx, storage = parse_Tree_Node(bytes_, idx, 0, 1)
     return idx, ("account_leaf", pathnibbles, address, balance, nonce, bytecode, storage)
   return idx, ("account_leaf", pathnibbles, address, balance, nonce)
 
-def gen_Account_Node(file_, account):
-  if verbose: print("gen_Account_Node",file_,account)
-  if len(account)==5:
+def gen_Account_Node(file_, leaf):
+  if len(leaf)==5:
     gen_Byte(file_, 0x00)
-  elif len(account)==7:
+  elif len(leaf)==7:
     gen_Byte(file_, 0x01)
-  gen_Nibbles(file_,account[1][1])
-  gen_Address(file_,account[2])
-  gen_Bytes32(file_,account[3])
-  gen_Bytes32(file_,account[4])
-  if len(account)>5:
-    gen_Bytecode(file_,account[5])
-    gen_Account_Storage_Tree_Node(file_,account[6])
+  gen_Nibbles(file_,leaf[1][1])
+  gen_Address(file_,leaf[2])
+  gen_Bytes32(file_,leaf[3])
+  gen_Bytes32(file_,leaf[4])
+  if len(leaf)>5:
+    gen_Bytecode(file_,leaf[5])
+    gen_Tree_Node(file_,leaf[6])
 
 def parse_Bytecode(bytes_, idx):
   idx, len_ = parse_U32(bytes_, idx)
@@ -307,120 +323,15 @@ def gen_Bytecode(file_, bytecode):
   for i in range(len_):
     gen_Byte(file_, bytecode[i])
 
-
-############################
-# account storage tree nodes
-
-def parse_Account_Storage_Tree_Node(bytes_, idx, d):
-  assert d<65
-  idx, nodetype = parse_Byte(bytes_, idx)
-  assert nodetype in {0x00,0x01,0x02,0x03}
-  if nodetype==0x00:
-    idx, branch = parse_Account_Storage_Branch_Node(bytes_, idx, d)
-    return idx, branch
-  elif nodetype==0x01:
-    idx, extension = parse_Account_Storage_Extension_Node(bytes_, idx, d)
-    return idx, extension
-  elif nodetype==0x02:
-    idx, account = parse_Account_Storage_Account_Node(bytes_, idx, d)
-    return idx, account
-  elif nodetype==0x03:
-    idx, hash_ = parse_Bytes32(bytes_, idx)
-    return idx, hash_
-
-def gen_Account_Storage_Tree_Node(file_, node):
-  if verbose: print("gen_Tree_Node",file_,node)
-  if node[0]=="branch":
-    gen_Byte(file_, 0x00)
-    gen_Account_Storage_Branch_Node(file_, node)
-  elif node[0]=="extension":
-    gen_Byte(file_, 0x01)
-    gen_Account_Storage_Extension_Node(file_, node)
-  elif node[0]=="account":
-    gen_Byte(file_, 0x02)
-    gen_Account_Storage_Leaf_Node(file_, node)
-  elif node[0]=="hash":
-    gen_Byte(file_, 0x03)
-    gen_Bytes32(file_, node[1])
-  else:
-    print("ERROR")
-
-def parse_Account_Storage_Branch_Node(bytes_, idx, depth):
-  assert depth < 64
-  idx, bitmask = parse_Bytes2_More_Than_One_Bit_Set(bytes_, idx)
-  bitmaskstr = bin(bitmask[0])[2:].zfill(8) + bin(bitmask[1])[2:].zfill(8)
-  c = [None]*16
-  for i in range(16):
-    if bitmaskstr[i]=='1':
-      idx, child = parse_Account_Storage_Tree_Node(bytes_, idx, depth)
-      c[i] = child
-  return idx, ("branch", c)
-
-def gen_Account_Storage_Branch_Node(file_,b):
-  if verbose: print("gen_Account_Storage_Branch_Node",file_,b)
-  # generate bitmask
-  bitmaskstr = ""
-  for branch in b[1:]:
-    if branch:
-      bitmaskstr += '1'
-    else:
-      bitmaskstr += '0'
-  bitmask = int(bitmaskstr,2).to_bytes(2,"big")
-  gen_Bytes2_More_Than_One_Bit_Set(file_,bitmask)
-  # generate children
-  for i in range(16):
-    if bitmaskstr[i]=='1':
-      gen_Account_Storage_Tree_Node(file_,b[i+1])
-
-def parse_Account_Storage_Extension_Node(bytes_, idx, depth):
-  assert depth < 63
-  idx, nibbleslen = parse_Byte(bytes_, idx)
-  idx, nibbles = parse_Nibbles(bytes_, idx, nibbleslen)
-  idx, child = parse_Child_Of_Account_Storage_Extension_Node(bytes_, idx, depth+nibbleslen)
-  # if odd number of nibbles, last one must be zeros
-  return idx, ("extension", (nibbleslen, nibbles), child)
-
-def gen_Account_Storage_Extension_Node(file_,e):
-  if verbose: print("gen_Extension_Node",file_,e)
-  gen_Byte(file_, e[1][0])
-  gen_Nibbles(file_, e[1][1])
-  gen_Child_Of_Account_Storage_Extension_Node(file_,e[2])
-
-def parse_Child_Of_Account_Storage_Extension_Node(bytes_, idx, depth):
-  if verbose: print("parse_Child_Of_Extension_Node",bytes_,idx,depth)
-  assert depth<65
-  idx, nodetype = parse_Byte(bytes_, idx)
-  assert nodetype in {0x00,0x03}
-  if nodetype==0x00:
-    idx, branch = parse_Account_Storage_Branch_Node(bytes_, idx, depth)
-    return idx, branch
-  elif nodetype==0x03:
-    idx, hash_ = parse_Bytes32(bytes_, idx)
-    return idx, hash_
-
-def gen_Child_Of_Extension_Node(file_, node):
-  if node[0]=="branch":
-    gen_Byte(file_, 0x00)
-    gen_Account_Storage_Branch_Node(file_, node)
-  elif node[0]=="hash":
-    gen_Byte(file_, 0x03)
-    gen_Bytes32(file_, node[1])
-
-def parse_Account_Storage_Leaf_Node(bytes_,idx, depth):
-  assert depth<65
-  idx, accounttype = parse_Byte(bytes_, idx)
-  assert accounttype in {0x00,0x01}
+def parse_Storage_Leaf_Node(bytes_,idx,depth):
   idx, pathnibbles = parse_Nibbles(bytes_,idx,64-depth)
   idx, key = parse_Bytes32(bytes_,idx)
   idx, value = parse_Bytes32(bytes_,idx)
   return idx, ("storage_leaf", pathnibbles, key, value)
 
-def gen_Account_Storage_Leaf_Node(file_, account):
-  if verbose: print("gen_Account_Storage_Leaf_Node",file_,account)
-  gen_Nibbles(file_,account[1])
-  gen_Bytes32(file_,account[2])
-  gen_Bytes32(file_,account[3])
-  if len(account)>4:
-    gen_Bytecode(file_,account[4])
-    gen_Account_Storage_Tree_Node(file_,account[5])
+def gen_Storage_Leaf_Node(file_, leaf):
+  gen_Nibbles(file_,leaf[1])
+  gen_Bytes32(file_,leaf[2])
+  gen_Bytes32(file_,leaf[3])
+
 
